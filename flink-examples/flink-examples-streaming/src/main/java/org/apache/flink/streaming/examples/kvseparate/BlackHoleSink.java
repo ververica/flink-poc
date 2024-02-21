9e717d3983f5a1c494beb1fc87259d62eec64e08
Jinzhong Li <lijinzhong2051@gmail.com>
2024-02-21 17:08:33 +0800

    Revert "Introduce Join Benchmark"
    This reverts commit 79450da2761adbef9904450df30fb5a04033709e.

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/kvseparate/BlackHoleSink.java
+++
@@ -1,8 +0,0 @@
-package org.apache.flink.streaming.examples.kvseparate;
-
-import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
-
-public class BlackHoleSink extends RichSinkFunction<String> {
-
-    private static final long serialVersionUID = 1L;
-}
